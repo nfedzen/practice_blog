0423427d07f6648c7b3a254b439db9905f2a81b7
Nathan Fedzen <nathanfedzen@gmail.com>
2021-05-11 01:15:22 -0400

    added register component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import Login from './user/Login.js'
 import Logout from './user/Logout.js'
+import Register from './user/Register.js'
 
 
 export default function App () {
-  return <Logout user="Nathan Fedzen" />
+  return <Register />
 }
+

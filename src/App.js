8e43992f0f3886027c72bb484740795665fc26e1
Nathan Fedzen <nathanfedzen@gmail.com>
2021-05-13 17:41:13 -0400

    added Post component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Post from './post/Post'
 import UserBar from './user/UserBar.js'
 
 
 export default function App () {
-  return <UserBar />
+  return <Post title="Hello" content="This is me playing around with hooks!!" author="Nathan Fedzen" />
 }
 

87f5cfe8242b60b2a2a2dd6c4623fea97af6cf82
Nathan Fedzen <nathanfedzen@gmail.com>
2021-05-14 16:39:09 -0600

    finished

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ import UserBar from './user/UserBar'
 
 
 
-const posts = [
+const defaultPosts = [
   { title: 'React Hooks', content: 'The greatest thing since sliced bread!', author: 'Daniel Bugl' },
   { title: 'Using React Fragments', content: 'Keeping the DOM tree clean!', author: 'Daniel Bugl' } 
 ]
 
 export default function App () {
   const [user, setUser] = useState('')
+  const [posts, setPosts] = useState(defaultPosts)
 
   return(
     <div style={{padding: 8}}>
       <UserBar user={user} setUser={setUser} />
       <br />
-      {user && <CreatePost user={user} />}
+      {user && <CreatePost user={user} posts={posts} setPosts={setPosts} />}
       <br />
       <hr />
       <PostList posts={posts} />
